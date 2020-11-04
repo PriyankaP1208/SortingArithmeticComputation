f44669f508df4664159ba7538413bcfec71d12ba
PriyankaP1208 <ppriyapatil1208@gmail.com>
2020-11-04 15:37:40 +0530

    Added file:To sort the result in Ascending Order

--- ArithmeticComputation.sh
+++ ArithmeticComputation.sh
@@ -44,3 +44,17 @@ do
     	done
 done
 echo "Array sorted in Descending order :${arr[*]}"
+
+for ((i = 0; i<5; i++))
+do
+	for((j = 0; j<5-i-1; j++))
+    	do
+        	if [[ ${arr[j]} -gt ${arr[$((j+1))]} ]]
+        	then
+            		temp=${arr[j]}
+            		arr[$j]=${arr[$((j+1))]}
+            		arr[$((j+1))]=$temp
+        	fi
+    	done
+done
+echo "Array sorted in Ascending order :${arr[*]}"
